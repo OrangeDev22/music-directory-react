d80dfc73c3496e6eeadd07989c1a654f6f026e70
OrangeDev22 <OrangeDev8@gmail.com>
2021-02-26 16:35:10 -0600

    adding firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,22 @@
 import firebase from "firebase";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
+const DB_URL = process.env.REACT_APP_DB_URL;
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const APP_ID = process.env.REACT_APP_APP_ID;
+const APP_MEASUREMENT_ID = process.env.REACT_APP_MEASUREMENT_ID;
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAiS2Pafdo6qeu6ru8bNfDRi6JO3oX6UfI",
-  authDomain: "music-directory-bd85f.firebaseapp.com",
-  databaseURL: "https://music-directory-bd85f-default-rtdb.firebaseio.com/",
-  projectId: "music-directory-bd85f",
-  storageBucket: "music-directory-bd85f.appspot.com",
-  messagingSenderId: "59114652878",
-  appId: "1:59114652878:web:4eb9edf916ca0931043c9f",
-  measurementId: "G-FWGH6SW7GL",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DB_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: APP_MEASUREMENT_ID,
 });
 
 const db = firebaseApp.firestore();
